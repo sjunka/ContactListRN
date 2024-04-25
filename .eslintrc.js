fa48e61551bbd1b67338a4a999e7cd30673a40c0
sergio junca <sergio.junca@gmail.com>
2024-04-25 18:48:01 -0500

    fix: linter, added screens, const, hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 module.exports = {
   root: true,
   extends: '@react-native',
+  eslintPluginPrettierRecommended,
 };
+
+
+
---
+++ src/screens/ContactsList.jsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function ContactsList() {
+  return (
+    <View>
+      <Text>ContactsList</Text>
+    </View>
+  );
+}
