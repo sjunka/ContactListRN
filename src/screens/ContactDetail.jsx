d7360ce3e658377e6c5549a5f82b68629630c170
sergio junca <sergio.junca@gmail.com>
2024-04-26 08:29:31 -0500

    feat: added routing with params for ContactDetail

--- src/screens/ContactDetail.jsx
+++ src/screens/ContactDetail.jsx
@@ -1,10 +1,11 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 
-export default function ContactDetail({navigation}) {
+export default function ContactDetail({route}) {
   return (
     <View>
       <Text>ContactDetail</Text>
+      <Text>{JSON.stringify(route)}</Text>
     </View>
   );
 }
