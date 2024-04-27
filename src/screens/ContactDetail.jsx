f987c474b38c908595155b5583303c0cd51c6119
sergio junca <sergio.junca@gmail.com>
2024-04-27 18:29:02 -0500

    feat: added contactDetail styles

--- src/screens/ContactDetail.jsx
+++ src/screens/ContactDetail.jsx
@@ -1,29 +1,172 @@
-import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  Image,
+} from 'react-native';
 import React from 'react';
 
-import HeaderWithNavigation from '../components/UI/HeaderWithNavigation';
+export default function ContactDetail({route, navigation}) {
+  const {
+    largeImageURL,
+    name,
+    emailAddress,
+    companyName,
+    birthdate,
+    phone,
+    address,
+  } = route.params.item;
+  console.log('🚀 ~ ContactDetail ~ largeImageURL:', largeImageURL);
+  console.log('🚀 ~ ContactDetail ~ address:', address);
+  console.log('🚀 ~ ContactDetail ~ phone:', phone);
+  console.log('🚀 ~ ContactDetail ~ birthdate:', birthdate);
+  console.log('🚀 ~ ContactDetail ~ companyName:', companyName);
+  console.log('🚀 ~ ContactDetail ~ emailAddress:', emailAddress);
+  console.log('🚀 ~ ContactDetail ~ name:', name);
 
-export default function ContactDetail({route}) {
   return (
-    <SafeAreaView style={styles.container}>
-      {/* <HeaderWithNavigation /> */}
-      <Text>{JSON.stringify(route)}</Text>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.containerTop}>
+          <View style={styles.imageContainer}>
+            <Image source={{uri: largeImageURL}} style={styles.image} />
+            <View style={styles.textWrapper}>
+              <Text style={styles.text}>{name}</Text>
+              {companyName ? (
+                <Text style={styles.text}>{companyName}</Text>
+              ) : null}
+            </View>
+          </View>
+        </View>
+        <View style={styles.horizontalLine} />
+        <View style={styles.containerMid}>
+          <View>
+            <Text style={styles.leftText}>Phone</Text>
+          </View>
+
+          <View style={styles.innerContainer}>
+            <Text style={styles.phoneText}>{phone.home}</Text>
+            <Text style={styles.mobileText}>Home</Text>
+          </View>
+          <View style={styles.horizontalLinePhone} />
+
+          <View style={styles.innerContainer}>
+            <Text style={styles.phoneText}>{phone.mobile}</Text>
+            <Text style={styles.mobileText}>Mobile</Text>
+          </View>
+          <View style={styles.horizontalLinePhone} />
+
+          <View style={styles.innerContainer}>
+            <Text style={styles.phoneText}>{phone.work}</Text>
+            <Text style={styles.mobileText}>Work</Text>
+          </View>
+          <View style={styles.horizontalLinePhone} />
+
+          <View style={styles.innerContainer}>
+            <Text style={styles.phoneText}>{phone.work}</Text>
+            <Text style={styles.mobileText}>Work</Text>
+          </View>
+          <View style={styles.horizontalLinePhone} />
+
+          <View>
+            <Text style={styles.leftText}>Address</Text>
+          </View>
+          <View style={styles.innerContainerAddress}>
+            <Text style={styles.addressText}>{address.street}</Text>
+          </View>
+          <View style={styles.innerContainerAddress}>
+            <Text
+              style={
+                styles.addressText
+              }>{`${address.city}, ${address.state} ${address.zipCode}, ${address.country}`}</Text>
+          </View>
+          <View style={styles.horizontalLinePhone} />
+
+          <View>
+            <Text style={styles.leftText}>Birthdate</Text>
+          </View>
+          <View style={styles.innerContainerAddress}>
+            <Text style={styles.addressText}>{birthdate}</Text>
+          </View>
+          <View style={styles.horizontalLinePhone} />
+
+          <View>
+            <Text style={styles.leftText}>Email</Text>
+          </View>
+          <View style={styles.innerContainerAddress}>
+            <Text style={styles.addressText}>{emailAddress}</Text>
+          </View>
+          <View style={styles.horizontalLinePhone} />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  loaderContainer: {
-    flex: 1,
+  containerTop: {
+    backgroundColor: 'white', // You can change the colors as per your requirement
     justifyContent: 'center',
     alignItems: 'center',
-    height: '100%',
-    backgroundColor: '#F8F8F8',
+    flex: 4,
+    paddingTop: 40,
+    paddingBottom: 30,
+  },
+  containerMid: {
+    backgroundColor: 'salmon', // You can change the colors as per your requirement
+    justifyContent: 'flex-start',
+    flex: 6,
+    marginHorizontal: 20,
+  },
+  textWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  text: {
+    color: 'black',
+    fontSize: 20,
+  },
+  image: {
+    width: 300,
+    height: 300,
+  },
+  horizontalLine: {
+    borderBottomWidth: 10,
+    // borderBottomColor: '#E5E4E2',
+    borderBottomColor: 'red',
+    marginHorizontal: 20,
   },
 
-  container: {
+  leftText: {
+    textAlign: 'left',
+    marginRight: 20, // Adjust spacing as needed
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between', // Aligns children with space between them
+  },
+  innerContainerAddress: {
+    flexDirection: 'row',
+    // alignItems: 'space-between', // Aligns children with space between them
+    // flexGrow: 1
+  },
+  addressText: {
+    alignSelf: 'flex-start', // Aligns text to the start (left)
     flex: 1,
-    flexDirection: 'column',
-    height: '100%',
+    color: 'black',
+    fontSize: 20,
+  },
+  phoneText: {
+    alignSelf: 'flex-start', // Aligns text to the start (left)
+  },
+  mobileText: {
+    alignSelf: 'flex-end', // Aligns text to the end (right)
+  },
+  horizontalLinePhone: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
   },
 });
